2e4cc5955755973c604fc385ca3b1cf047905e33
ErichGiusseppe <eg.soto@uniandes.edu.co>
2023-04-19 00:51:42 -0500

    errores arreglados

--- pipeline/proce.py
+++ pipeline/proce.py
@@ -37,26 +37,26 @@ class TextPreprocessor(BaseEstimator, TransformerMixin):
         # convert series to dataframe
         movies_df = pd.DataFrame(df)
 
+
         # Descargando las stopwords
         nltk.download('stopwords')
         stop_words = list(stopwords.words('spanish'))
         stop_words_e = list(stopwords.words('english'))
-            
+
         # Eliminando caracteres no alfanumericos y pasamos los caracteres a minusculas
         movies_df['review_es'] = movies_df['review_es'].apply(lambda x: re.sub(r'\W+', ' ', x).lower())
-        
+
         # Eliminamos las stopwords de español y ingles
         movies_df['review_es'] = movies_df['review_es'].apply(lambda x: " ".join([word for word in x.split() if word not in stop_words]))
         movies_df['review_es'] = movies_df['review_es'].apply(lambda x: " ".join([word for word in x.split() if word not in stop_words_e]))
 
         #Eliminamos las palabras que no pertenecen al Español ni Inglés
         lista_correcciones =  []
-        for index, value in movies_df['review_es'].iteritems():
+        for value in movies_df['review_es']:
             for word in self.verificar_palabra(value):
                 lista_correcciones.append(word)
-
-        movies_df['review_es'] = movies_df['review_es'].apply(lambda x: re.sub(r'\b(' + '|'.join(lista_correcciones) + r')\b', x))
-        
+        text=r'\b(' + '|'.join(lista_correcciones) + r')\b'
+        movies_df['review_es'] = movies_df['review_es'].apply(lambda x: re.sub(text,' ', x))
 
         #Remover las palabras con poca frecuencia de apariciones
         word_count = Counter()
@@ -69,13 +69,12 @@ class TextPreprocessor(BaseEstimator, TransformerMixin):
         # Remover las palabras con alta frecuencia de apariciones
         FREQUENT_WORDS = set(word for (word, wc) in word_count.most_common(10))  
         movies_df['review_es']= movies_df['review_es'].apply(lambda x: " ".join([word for word in x.split() if word not in FREQUENT_WORDS]))
-
         
         # Eliminamos las tildes
         movies_df['review_es'] = movies_df['review_es'].apply(lambda x: ''.join(c for c in (unicodedata.normalize('NFD', x)) if unicodedata.category(c) != 'Mn'))
         print("data: ",movies_df)
-        
         movies = movies_df['review_es']
-
         print("Finished preprocessing text...")
+
+
         return movies
